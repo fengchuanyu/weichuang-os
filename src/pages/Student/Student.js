0ecc093fd351987949c2b69064c32e1548805e67
gonglixin <1480059245@qq.com>
2019-03-12 20:36:10 +0800

    test

--- src/pages/Student/Student.js
+++ src/pages/Student/Student.js
@@ -24,7 +24,7 @@ class Student extends Component {
   render() {
     return (
       <div>
-        <PageHeaderWrapper title="学生列表">学生管理</PageHeaderWrapper>
+        <PageHeaderWrapper title="学生列表">学生管学生理</PageHeaderWrapper>
       </div>
     );
   }
